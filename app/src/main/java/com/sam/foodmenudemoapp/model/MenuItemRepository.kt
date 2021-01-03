280aee6800b2a13c047dd50216929d4d6abe3f53
shyam <radheshyam.srinivasan@gmail.com>
2021-01-04 02:52:57 +0800

    Update MenuItemRepository.kt
    Added drinks and sushi items

--- app/src/main/java/com/sam/foodmenudemoapp/model/MenuItemRepository.kt
+++ app/src/main/java/com/sam/foodmenudemoapp/model/MenuItemRepository.kt
@@ -22,7 +22,27 @@ class MenuItemRepository {
                 itemName = "Chicago Style",
                 itemDescription = "Pizza prepared according to several different styles developed in Chicago. The most famous is the deep-dish pizza.",
                 itemImageLocation = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Chicago-style_pizza.jpg/1920px-Chicago-style_pizza.jpg",
-                itemCategory = MenuItemCategory.PIZZA )
+                itemCategory = MenuItemCategory.PIZZA ),
+            MenuItem(
+                itemName = "Nigiri",
+                itemDescription = "Consists of thinly sliced, raw fish pressed on top of sushi rice",
+                itemImageLocation = "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2003/10/27/0/ad1a10_sushi.jpg.rend.hgtvcom.826.620.suffix/1371583036385.jpeg",
+                itemCategory = MenuItemCategory.SUSHI ),
+            MenuItem(
+                itemName = "Sashimi",
+                itemDescription = "Sashimi consists of thin slices of raw fish either salmon or tuna.",
+                itemImageLocation = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Sashimi_combo_%2830122297838%29.jpg/1920px-Sashimi_combo_%2830122297838%29.jpg",
+                itemCategory = MenuItemCategory.SUSHI ),
+            MenuItem(
+                itemName = "Lime Juice",
+                itemDescription = "The lime rickey is a refreshingly tart combination of fresh lime juice, simple syrup, and seltzer. ",
+                itemImageLocation = "https://www.thespruceeats.com/thmb/2hjFZTZ9xr7TlLaQ22hY3t-9rc0=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/lime-rickey-908792-hero-5c25321646e0fb0001ebbbdf.jpg",
+                itemCategory = MenuItemCategory.DRINKS ),
+            MenuItem(
+                itemName = "Banana Milkshake",
+                itemDescription = "Fruit milkshakes or smoothies are very popular",
+                itemImageLocation = "https://149366112.v2.pressablecdn.com/wp-content/uploads/2019/03/vegan-golden-banana-milkshake-1-949x800.jpg",
+                itemCategory = MenuItemCategory.DRINKS )
 
         ))
         menuItems
